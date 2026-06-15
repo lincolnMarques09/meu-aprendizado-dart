90fad5157cd35a347b890e7305c36944614f6ae5
Lincoln Marques <lincolnmota09@gmail.com>
2026-06-15 14:06:30 -0300

    Meios de percorrer uma lista
    Testando meios de percorrer listas no dart.

--- fase-1-estrutura-de-dados/lista.dart
+++ fase-1-estrutura-de-dados/lista.dart
@@ -1,13 +1,21 @@
 void main(){
   List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
 
+  //For in: maneira mais prática de percorrer *TODOS* os elementos da lista.
   for(int numero in numeros){
     print(numero);
   }
 
+  //For clássico: serve para manipulação dos valores e índices.
   for(int i = 0; i < numeros.length; i++){
     print(numeros[i]);
   }
 
-  print(numeros);
+  //For each: método presente nas listas que permite percorrer os elementos de maneira mais simples e compacta.
+  numeros.forEach((numero){
+    print('Número da vez: $numero');
+  });
+
+  //For each 2: Uma alternativa mais simples, é usar o arrow function para poupar linhas.
+  numeros.forEach((numero) => print(numero));
 }

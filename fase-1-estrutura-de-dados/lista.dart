2760c0650e4b06f67cef2fa449ee44746a0b3125
Lincoln Marques <lincolnmota09@gmail.com>
2026-06-15 13:55:30 -0300

    Percorrendo listas no dart.
    Testando maneiras de percorres os valores na lista.

---
+++ fase-1-estrutura-de-dados/lista.dart
@@ -0,0 +1,13 @@
+void main(){
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+
+  for(int numero in numeros){
+    print(numero);
+  }
+
+  for(int i = 0; i < numeros.length; i++){
+    print(numeros[i]);
+  }
+
+  print(numeros);
+}

0a2d985c816afb40c8830363fec4e3385fcac172
Lincoln Marques <lincolnmota09@gmail.com>
2026-06-15 22:06:27 -0300

    Introdução a mapas
    Aprendendo os mapas

---
+++ fase-1-estrutura-de-dados/mapas/mapa_conceito.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  //Introdução a mapas:
+  //Outra estrutura de dados. Contém uma chave e valor, e seus tipos são definidos entre os <>. 
+  //O tipo 'dynamic' permite que o mapa recebe qualquer tipo de dado como valor.
+
+  Map<String, dynamic> usuario = {
+    'nome': 'Lincoln',
+    'idade': 17,
+    'dev_dart': true
+  };
+
+  print(usuario);
+}
